de995063d314dd1c2fabb1b0dfe9dd8d201c5f25
Manohar <manoharchoudaryv@gmail.com>
2020-09-22 15:02:39 +0530

    service created and regform

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,13 +9,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm:FormGroup;
   regForm:FormGroup;
+  isLogin:boolean = true;
   constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.initiateLFOrm(); 
+    this.initiateRFOrm(); 
+  }
+
+
+  initiateLFOrm(){
     this.loginForm = this.fb.group({
       username:['',Validators.required],
       password:['',Validators.required]
-    })
+    });
+  }
+
+  initiateRFOrm(){
+    this.regForm = this.fb.group({
+      email:['',Validators.required],
+      password:['',Validators.required],
+      c_pwd:['',Validators.required]
+    });
+  }
+
+  onLogin(fd){
+    console.log(fd);
+  }
+
+  onRegestration(fd){
+    console.log(fd);
   }
 
 }
---
+++ src/app/shared/language.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+
+  constructor() { }
+}
