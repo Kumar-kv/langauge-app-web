41b4954a9f23710ddd55b19a75e2f61d3fae9937
Manohar <manoharchoudaryv@gmail.com>
2020-10-27 15:01:33 +0530

    validations

--- src/app/admin/numbers/numbers.component.ts
+++ src/app/admin/numbers/numbers.component.ts
@@ -10,6 +10,7 @@ import { LanguageService } from 'src/app/shared/language.service';
 export class NumbersComponent implements OnInit {
   numbersForm:FormGroup;
   isEdit:boolean = false;
+  submitted:boolean = false;
   constructor(
     private ls:LanguageService,
     private fb:FormBuilder
@@ -23,11 +24,30 @@ export class NumbersComponent implements OnInit {
     this.numbersForm = this.fb.group({
       english:['',Validators.required],
       target_translation:['',Validators.required]
-    })
+    });
+    console.log(this.numbersForm.controls);
   }
 
+get f(){
+  return this.numbersForm.controls;
+}
+
   onSubmit(){
-    
+    this.submitted = true;
+    if(this.numbersForm.invalid){
+      return;
+    }
+    let fd:any=  this.numbersForm.value;
+    for(let p in fd){
+        fd[p] = fd[p].trim().toLowerCase();
+    }
+    console.log(fd);
+    return;
+    this.ls.postCall(fd,"numbers").subscribe(res=>{
+      console.log(res);
+      this.numbersForm.reset();
+      this.submitted = false;
+    })
   }
 
 
